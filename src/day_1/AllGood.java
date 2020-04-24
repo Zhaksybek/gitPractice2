6436a36383b21c3634f77af2361f6b67634242e3
Zhaksybek <zhaks18121993@gmail.com>
2020-04-24 12:03:28 -0700

    my best commit :))

---
+++ src/day_1/AllGood.java
@@ -0,0 +1,10 @@
+package day_1;
+
+public class AllGood {
+    // From todays day InshalAllah everything will good!
+
+    public static void main(String[] args) {
+        System.out.println("Hello bro!");
+        System.out.println("I am ok, U know, I am ok! :))");
+    }
+}
