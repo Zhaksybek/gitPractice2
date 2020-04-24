065d269664d67d6406042f64fec92cc222476a94
Zhaksybek <62046646+Zhaksybek@users.noreply.github.com>
2020-04-24 13:30:16 -0700

    Update AllGood2.java

--- src/day_1/AllGood2.java
+++ src/day_1/AllGood2.java
@@ -4,6 +4,9 @@ package day_1;
 Hello guys; All good, all good!
 */
 public class AllGood2 {
+    
+    // This line comment was added by Github!
+    
     // From todays day InshalAllah everything will good!
 
     public static void main(String[] args) {
