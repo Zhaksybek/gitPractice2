a9b3570d508a0b405ba3a5f944aad39562c37ff8
Zhaksybek <zhaks18121993@gmail.com>
2020-05-08 11:28:32 -0700

    git ignore

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -6,5 +6,6 @@ public class AllGood {
     public static void main(String[] args) {
         System.out.println("Hello bro!");
         System.out.println("I am ok, U know, I am ok! :))");
+        // hi
     }
 }
